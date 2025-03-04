2943bb7ef2446da7669ab9fb61103d223a3a86c4
Pierre-Henri Symoneaux <pierre-henri.symoneaux@ovhcloud.com>
2025-03-04 09:45:29 +0100

    feat(client): add encrypt / decrypt helpers
    Signed-off-by: Pierre-Henri Symoneaux <pierre-henri.symoneaux@ovhcloud.com>

---
+++ examples/encrypt_decrypt.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"slices"
+
+	"github.com/ovh/kmip-go"
+	"github.com/ovh/kmip-go/kmipclient"
+	"github.com/ovh/kmip-go/payloads"
+)
+
+func test_encrypt_decrypt_aes(client *kmipclient.Client) {
+	iv := []byte("abcdefghijkl")
+	plain := []byte("Hello World!")
+	aad := []byte("toto")
+
+	keyId := client.Create().
+		AES(256, kmip.Encrypt|kmip.Decrypt).
+		MustExec().
+		UniqueIdentifier
+
+	client.Activate(keyId).MustExec()
+
+	resp := client.Encrypt(keyId).
+		WithIvCounterNonce(iv).
+		WithAAD(aad).
+		WithCryptographicParameters(kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.GCM),
+		}).
+		Data(plain).
+		MustExec()
+
+	client.Decrypt(keyId).
+		WithIvCounterNonce(iv).
+		WithAAD(aad).
+		WithCryptographicParameters(kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.GCM),
+		}).
+		WithAuthTag(*resp.AuthenticatedEncryptionTag).
+		Data(resp.Data).
+		MustExec()
+}
+
+func test_encrypt_decrypt_aes_default(client *kmipclient.Client) {
+	plain := []byte("Hello World!")
+	aad := []byte("toto")
+
+	keyId := client.Create().
+		AES(256, kmip.Encrypt|kmip.Decrypt).
+		WithAttribute(kmip.AttributeNameCryptographicParameters, kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.GCM),
+			RandomIV:               ptrTo(true),
+		}).
+		MustExec().
+		UniqueIdentifier
+
+	client.Activate(keyId).MustExec()
+
+	resp := client.Encrypt(keyId).
+		WithAAD(aad).
+		Data(plain).
+		MustExec()
+
+	client.Decrypt(keyId).
+		WithIvCounterNonce(*resp.IVCounterNonce).
+		WithAAD(aad).
+		WithAuthTag(*resp.AuthenticatedEncryptionTag).
+		Data(resp.Data).
+		MustExec()
+}
+
+func test_encrypt_encrypt_aes_cbc_pkcs5(client *kmipclient.Client) {
+	keyId := client.Create().AES(256, kmip.Encrypt|kmip.Decrypt).
+		WithName("Test-Encrypt-CBC").
+		MustExec().
+		UniqueIdentifier
+
+	client.Activate(keyId).MustExec()
+
+	iv := make([]byte, 16)
+	rand.Reader.Read(iv[:])
+
+	respPl, err := client.Request(context.Background(), &payloads.EncryptRequestPayload{
+		UniqueIdentifier: &keyId,
+		Data:             []byte("Hello World!"),
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.CBC),
+			PaddingMethod:          ptrTo(kmip.PKCS5),
+		},
+		IVCounterNonce: ptrTo(iv[:]),
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp := respPl.(*payloads.EncryptResponsePayload)
+
+	respD, err := client.Request(context.Background(), &payloads.DecryptRequestPayload{
+		UniqueIdentifier: &keyId,
+		Data:             resp.Data,
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.CBC),
+			PaddingMethod:          ptrTo(kmip.PKCS5),
+		},
+		IVCounterNonce: resp.IVCounterNonce,
+	})
+	if err != nil {
+		panic(err)
+	}
+	if !slices.Equal(respD.(*payloads.DecryptResponsePayload).Data, []byte("Hello World!")) {
+		panic("unexpected decryption output")
+	}
+}
+
+func test_encrypt_decrypt_rsa_oaep(client *kmipclient.Client) {
+	key := client.CreateKeyPair().RSA(2048, kmip.Decrypt, kmip.Encrypt).
+		Common().
+		WithAttribute(kmip.AttributeNameState, kmip.StateActive).
+		MustExec()
+
+	respPl, err := client.Request(context.Background(), &payloads.EncryptRequestPayload{
+		UniqueIdentifier: &key.PublicKeyUniqueIdentifier,
+		Data:             []byte("Hello World!"),
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.RSA),
+			PaddingMethod:          ptrTo(kmip.OAEP),
+			HashingAlgorithm:       ptrTo(kmip.SHA_256),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp := respPl.(*payloads.EncryptResponsePayload)
+
+	_, err = client.Request(context.Background(), &payloads.DecryptRequestPayload{
+		UniqueIdentifier: &key.PrivateKeyUniqueIdentifier,
+		Data:             resp.Data,
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.RSA),
+			PaddingMethod:          ptrTo(kmip.OAEP),
+			HashingAlgorithm:       ptrTo(kmip.SHA_256),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func test_encrypt_decrypt_rsa_pkcs1(client *kmipclient.Client) {
+	key := client.CreateKeyPair().RSA(2048, kmip.Decrypt, kmip.Encrypt).
+		Common().
+		WithAttribute(kmip.AttributeNameState, kmip.StateActive).
+		MustExec()
+
+	respPl, err := client.Request(context.Background(), &payloads.EncryptRequestPayload{
+		UniqueIdentifier: &key.PublicKeyUniqueIdentifier,
+		Data:             []byte("Hello World!"),
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.RSA),
+			PaddingMethod:          ptrTo(kmip.PKCS1V1_5),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp := respPl.(*payloads.EncryptResponsePayload)
+
+	_, err = client.Request(context.Background(), &payloads.DecryptRequestPayload{
+		UniqueIdentifier: &key.PrivateKeyUniqueIdentifier,
+		Data:             resp.Data,
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.RSA),
+			PaddingMethod:          ptrTo(kmip.PKCS1V1_5),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func test_encrypt_decrypt_aes_with_usage(client *kmipclient.Client) {
+	keyId := client.Create().AES(256, kmip.Encrypt|kmip.Decrypt).
+		WithUsageLimit(1, kmip.UsageLimitsUnitByte).
+		MustExec().
+		UniqueIdentifier
+
+	client.Activate(keyId).MustExec()
+
+	respPl, err := client.Request(context.Background(), &payloads.EncryptRequestPayload{
+		UniqueIdentifier: &keyId,
+		Data:             []byte("Hello World!"),
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.GCM),
+		},
+		IVCounterNonce:                        ptrTo([]byte("abcdefghijkl")),
+		AuthenticatedEncryptionAdditionalData: ptrTo([]byte("toto")),
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp := respPl.(*payloads.EncryptResponsePayload)
+
+	_, err = client.Request(context.Background(), &payloads.DecryptRequestPayload{
+		UniqueIdentifier: &keyId,
+		Data:             resp.Data,
+		CryptographicParameters: &kmip.CryptographicParameters{
+			CryptographicAlgorithm: ptrTo(kmip.AES),
+			BlockCipherMode:        ptrTo(kmip.GCM),
+		},
+		IVCounterNonce:                        resp.IVCounterNonce,
+		AuthenticatedEncryptionTag:            resp.AuthenticatedEncryptionTag,
+		AuthenticatedEncryptionAdditionalData: ptrTo([]byte("toto")),
+	})
+	if err != nil {
+		panic(err)
+	}
+}
