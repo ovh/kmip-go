9e35bdeefb547e6a7f3f2bb8f4dd8f56920299ec
Pierre-Henri Symoneaux <pierre-henri.symoneaux@ovhcloud.com>
2025-03-13 13:47:45 +0100

    feat(client): add crypto.Signer wrapper
    This enables integration into golang's TLS stack
    or in x509 signing code
    
    Signed-off-by: Pierre-Henri Symoneaux <pierre-henri.symoneaux@ovhcloud.com>

--- examples/sign_verify.go
+++ examples/sign_verify.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+
 	"github.com/ovh/kmip-go"
 	"github.com/ovh/kmip-go/kmipclient"
 )
 
-func test_sign_verify_rsa(client *kmipclient.Client) {
+func test_sign_verify_rsa_pkcs1_15(client *kmipclient.Client) {
 	data := []byte("foobarbaz")
 	cparams := kmip.CryptographicParameters{
 		DigitalSignatureAlgorithm: kmip.DigitalSignatureAlgorithmSHA_256WithRSAEncryptionPKCS_1v1_5,
+		// CryptographicAlgorithm: kmip.CryptographicAlgorithmRSA,
+		// HashingAlgorithm:       kmip.HashingAlgorithmSHA_256,
+		// PaddingMethod:          kmip.PaddingMethodPKCS1V1_5,
 	}
 
 	key := client.CreateKeyPair().RSA(2048, kmip.CryptographicUsageSign, kmip.CryptographicUsageVerify).
-		Common().WithName("Test-Encrypt-RSA").
+		Common().WithName("Test-Sign-RSA-2").
 		WithAttribute(kmip.AttributeNameState, kmip.StateActive).
 		MustExec()
 
@@ -26,4 +34,75 @@ func test_sign_verify_rsa(client *kmipclient.Client) {
 		Data(data).
 		Signature(resp.SignatureData).
 		MustExec()
+}
+
+func test_sign_verify_rsa_pss(client *kmipclient.Client) {
+	data := []byte("foobarbaz")
+	cparams := kmip.CryptographicParameters{
+		DigitalSignatureAlgorithm: kmip.DigitalSignatureAlgorithmRSASSA_PSSPKCS_1v2_1,
+		// CryptographicAlgorithm: kmip.CryptographicAlgorithmRSA,
+		HashingAlgorithm: kmip.HashingAlgorithmSHA_256,
+		// PaddingMethod:          kmip.PaddingMethodPSS,
+	}
+
+	key := client.CreateKeyPair().RSA(2048, kmip.CryptographicUsageSign, kmip.CryptographicUsageVerify).
+		Common().WithName("Test-Sign-RSA-3").
+		WithAttribute(kmip.AttributeNameState, kmip.StateActive).
+		MustExec()
+
+	resp := client.Sign(key.PrivateKeyUniqueIdentifier).
+		WithCryptographicParameters(cparams).
+		Data(data).
+		MustExec()
+
+	client.SignatureVerify(key.PublicKeyUniqueIdentifier).
+		WithCryptographicParameters(cparams).
+		Data(data).
+		Signature(resp.SignatureData).
+		MustExec()
+}
+
+func test_sign_verify_ecdsa(client *kmipclient.Client) {
+	data := []byte("foobarbaz")
+	cparams := kmip.CryptographicParameters{
+		DigitalSignatureAlgorithm: kmip.DigitalSignatureAlgorithmECDSAWithSHA256,
+	}
+
+	key := client.CreateKeyPair().ECDSA(kmip.RecommendedCurveP_256, kmip.CryptographicUsageSign, kmip.CryptographicUsageVerify).
+		Common().WithName("Test-Sign-ECDSA").
+		WithAttribute(kmip.AttributeNameState, kmip.StateActive).
+		MustExec()
+
+	resp := client.Sign(key.PrivateKeyUniqueIdentifier).
+		WithCryptographicParameters(cparams).
+		Data(data).
+		MustExec()
+
+	client.SignatureVerify(key.PublicKeyUniqueIdentifier).
+		WithCryptographicParameters(cparams).
+		Data(data).
+		Signature(resp.SignatureData).
+		MustExec()
+}
+
+func test_crypto_signer_rsa_pkcs1_15(client *kmipclient.Client) {
+	key := client.CreateKeyPair().RSA(2048, kmip.CryptographicUsageSign, kmip.CryptographicUsageVerify).
+		Common().WithName("Test-Sign-RSA-4").
+		WithAttribute(kmip.AttributeNameState, kmip.StateActive).
+		MustExec()
+
+	signer, err := client.Signer(key.PrivateKeyUniqueIdentifier)
+	if err != nil {
+		panic(err)
+	}
+	data := []byte("hello world")
+	digest := sha256.Sum256(data)
+	sig, err := signer.Sign(rand.Reader, digest[:], crypto.SHA256)
+	if err != nil {
+		panic(err)
+	}
+	err = rsa.VerifyPKCS1v15(signer.Public().(*rsa.PublicKey), crypto.SHA256, digest[:], sig)
+	if err != nil {
+		panic(err)
+	}
 }
--- kmipclient/sign_verify.go
+++ kmipclient/sign_verify.go
@@ -1,8 +1,20 @@
 package kmipclient
 
 import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rsa"
+	"encoding/asn1"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"math/big"
+
 	"github.com/ovh/kmip-go"
 	"github.com/ovh/kmip-go/payloads"
+	"github.com/ovh/kmip-go/ttlv"
 )
 
 type ExecSign struct {
@@ -103,5 +115,239 @@ func (ex ExecSignatureVerifyWantsSignature) Signature(sig []byte) ExecSignatureV
 			client: ex.client,
 			req:    ex.req,
 		},
+	}
+}
+
+// Signer returns a crypto.Signer implementation using the remote private key for signing.
+// The public key must be non sensitive and extractable. The private key must be linked to its publickey.
+func (c *Client) Signer(privateKeyId string) (crypto.Signer, error) {
+	signer := &cryptoSigner{
+		client:       c,
+		privateKeyId: privateKeyId,
+	}
+
+	// Get key attributes and verify:
+	//        - Key is a private key object
+	//        - Key has Sign usage mask
+	//        - Has public key link
+	//        - Has supported algorithm
+	pubKeyId, err := signer.verifySignerKeyAttributes(privateKeyId, kmip.ObjectTypePrivateKey, kmip.CryptographicUsageSign)
+	if err != nil {
+		return nil, err
+	}
+	if pubKeyId == "" {
+		return nil, fmt.Errorf("failed to find public key ID")
+	}
+	// Check public key attributes:
+	//		  - Key is public key
+	//        - Key has same algorithm than private key
+	//        - Key has Verify usage mask
+	if _, err := signer.verifySignerKeyAttributes(pubKeyId, kmip.ObjectTypePublicKey, kmip.CryptographicUsageVerify); err != nil {
+		return nil, err
+	}
+
+	// Get and save public key material
+	resp, err := c.Get(pubKeyId).Exec()
+	if err != nil {
+		return nil, err
+	}
+	signer.publicKey, err = resp.PublicKey()
+	if err != nil {
+		return nil, err
+	}
+
+	return signer, nil
+}
+
+type cryptoSigner struct {
+	alg          kmip.CryptographicAlgorithm
+	privateKeyId string
+	publicKey    crypto.PublicKey
+	client       *Client
+}
+
+// Public implements crypto.Signer.
+func (c *cryptoSigner) Public() crypto.PublicKey {
+	return c.publicKey
+}
+
+func hashToKmip(h crypto.Hash) (kmip.HashingAlgorithm, error) {
+	switch h {
+	case crypto.SHA256:
+		return kmip.HashingAlgorithmSHA_256, nil
+	case crypto.SHA384:
+		return kmip.HashingAlgorithmSHA_384, nil
+	case crypto.SHA512:
+		return kmip.HashingAlgorithmSHA_512, nil
+	default:
+		return 0, errors.New("unsupported hash function")
+	}
+}
+
+// Sign implements crypto.Signer.
+func (c *cryptoSigner) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) (signature []byte, err error) {
+	if opts == nil {
+		return nil, errors.New("opts cannot be nil")
+	}
+	hashAlg, err := hashToKmip(opts.HashFunc())
+	if err != nil {
+		return nil, err
+	}
+
+	cparams := kmip.CryptographicParameters{
+		CryptographicAlgorithm: c.alg,
+		HashingAlgorithm:       hashAlg,
+	}
+	if c.alg == kmip.CryptographicAlgorithmRSA {
+		// When using RSA, default to PKCS1V1_5 padding.
+		cparams.PaddingMethod = kmip.PaddingMethodPKCS1V1_5
+	}
+	if pss, ok := opts.(*rsa.PSSOptions); ok {
+		// Then if opts in a PSSOptions, then overwrite padding method with PSS.
+		if c.alg != kmip.CryptographicAlgorithmRSA {
+			return nil, errors.New("rsa.PSSOptions can only be used with RSA keys")
+		}
+		cparams.PaddingMethod = kmip.PaddingMethodPSS
+		if pss.SaltLength > 0 && pss.SaltLength < math.MaxInt32 {
+			//XXX: Maybe differentiate between rsa.PSSSaltLengthEqualsHash and rsa.PSSSaltLengthAuto.
+			//     Not sure what the KMIP server would choose by default.
+			saltLength := int32(pss.SaltLength)
+			cparams.SaltLength = &saltLength
+		} else if pss.SaltLength == rsa.PSSSaltLengthEqualsHash {
+			saltLength := int32(opts.HashFunc().Size())
+			cparams.SaltLength = &saltLength
+		}
+	}
+
+	normalizeDSACryptoParams(&cparams)
+
+	resp, err := c.client.Sign(c.privateKeyId).
+		WithCryptographicParameters(cparams).
+		DigestedData(digest).
+		Exec()
+	if err != nil {
+		return nil, err
+	}
+
+	// As the KMIP standard does not specify which signature format is returned for ECDSA signatures,
+	// we must check which format is returned, and convert to ASN.1 if it's a raw signatures (concatenation of R and S).
+	if c.alg == kmip.CryptographicAlgorithmEC || c.alg == kmip.CryptographicAlgorithmECDSA {
+		// Check of signature is raw concatenation of r and s, or if it's ASN.1.
+		// One way to do it is to check the signature size. If it's exactly 2 times the size of curve size
+		// then it's raw. ANS.1 will always have more bytes.
+		curve := c.publicKey.(*ecdsa.PublicKey).Curve
+		if len(resp.SignatureData) == 2*((curve.Params().BitSize+7)/8) {
+			// Need to convert to ASN.1
+			resp.SignatureData, err = convertRawECDSAToASN1DER(resp.SignatureData, curve)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return resp.SignatureData, nil
+}
+
+func (c *cryptoSigner) verifySignerKeyAttributes(id string, expectedObjectType kmip.ObjectType, expectedUsageMask kmip.CryptographicUsageMask) (string, error) {
+	resp, err := c.client.GetAttributes(id,
+		kmip.AttributeNameObjectType,
+		kmip.AttributeNameCryptographicAlgorithm,
+		kmip.AttributeNameLink,
+		kmip.AttributeNameCryptographicUsageMask,
+	).Exec()
+	if err != nil {
+		return "", err
+	}
+
+	pubKeyId := ""
+
+	for _, attr := range resp.Attribute {
+		switch attr.AttributeName {
+		case kmip.AttributeNameObjectType:
+			if ot := attr.AttributeValue.(kmip.ObjectType); ot != expectedObjectType {
+				return "", fmt.Errorf("unexpected object type (got %s, wants %s)", ttlv.EnumStr(ot), ttlv.EnumStr(expectedObjectType))
+			}
+		case kmip.AttributeNameCryptographicAlgorithm:
+			// Save private key algorithm
+			alg := attr.AttributeValue.(kmip.CryptographicAlgorithm)
+			if alg != kmip.CryptographicAlgorithmRSA && alg != kmip.CryptographicAlgorithmEC && alg != kmip.CryptographicAlgorithmECDSA {
+				return "", fmt.Errorf("unsupported cryptographic algorithm %s", ttlv.EnumStr(alg))
+			}
+			if expectedObjectType == kmip.ObjectTypePrivateKey {
+				c.alg = alg
+			} else if alg != c.alg {
+				return "", fmt.Errorf("invalid cryptographic algorithm %s", ttlv.EnumStr(alg))
+			}
+
+		case kmip.AttributeNameLink:
+			// Get public key id
+			if expectedObjectType != kmip.ObjectTypePrivateKey {
+				continue
+			}
+			if ln := attr.AttributeValue.(kmip.Link); ln.LinkType == kmip.LinkTypePublicKeyLink {
+				pubKeyId = ln.LinkedObjectIdentifier
+			}
+		case kmip.AttributeNameCryptographicUsageMask:
+			if cum := attr.AttributeValue.(kmip.CryptographicUsageMask); cum&expectedUsageMask == 0 {
+				return "", fmt.Errorf("Unexpected usage mask (got %s, wants %s)", ttlv.BitmaskStr(cum, "|"), ttlv.BitmaskStr(expectedUsageMask, "|"))
+			}
+		}
+	}
+	return pubKeyId, nil
+}
+
+// convertRawECDSAToASN1DER converts a raw ECDSA signature (r || s) into ASN.1 DER format.
+func convertRawECDSAToASN1DER(rawSig []byte, curve elliptic.Curve) ([]byte, error) {
+	//TODO: Change maybe to use x/crypto's cryptobyte package which would be more permfromant.
+
+	// ECDSASignature represents the ASN.1 structure of an ECDSA signature
+	type ECDSASignature struct {
+		R, S *big.Int
+	}
+
+	// Ensure the raw signature length is correct
+	orderSize := (curve.Params().BitSize + 7) / 8 // Curve order size in bytes
+	if len(rawSig) != 2*orderSize {
+		return nil, fmt.Errorf("invalid raw signature length: expected %d, got %d", 2*orderSize, len(rawSig))
+	}
+
+	// Split raw signature into r and s
+	r := new(big.Int).SetBytes(rawSig[:orderSize])
+	s := new(big.Int).SetBytes(rawSig[orderSize:])
+
+	// Encode as ASN.1
+	der, err := asn1.Marshal(ECDSASignature{R: r, S: s})
+	if err != nil {
+		return nil, err
+	}
+
+	return der, nil
+}
+
+func normalizeDSACryptoParams(cparams *kmip.CryptographicParameters) {
+	switch cparams.CryptographicAlgorithm {
+	case kmip.CryptographicAlgorithmRSA:
+		switch cparams.PaddingMethod {
+		case kmip.PaddingMethodPKCS1V1_5:
+			switch cparams.HashingAlgorithm {
+			case kmip.HashingAlgorithmSHA_256:
+				cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmSHA_256WithRSAEncryptionPKCS_1v1_5
+			case kmip.HashingAlgorithmSHA_384:
+				cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmSHA_384WithRSAEncryptionPKCS_1v1_5
+			case kmip.HashingAlgorithmSHA_512:
+				cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmSHA_512WithRSAEncryptionPKCS_1v1_5
+			}
+		case kmip.PaddingMethodPSS:
+			cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmRSASSA_PSSPKCS_1v2_1
+		}
+	case kmip.CryptographicAlgorithmEC, kmip.CryptographicAlgorithmECDSA:
+		switch cparams.HashingAlgorithm {
+		case kmip.HashingAlgorithmSHA_256:
+			cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmECDSAWithSHA256
+		case kmip.HashingAlgorithmSHA_384:
+			cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmECDSAWithSHA384
+		case kmip.HashingAlgorithmSHA_512:
+			cparams.DigitalSignatureAlgorithm = kmip.DigitalSignatureAlgorithmECDSAWithSHA512
+		}
 	}
 }
